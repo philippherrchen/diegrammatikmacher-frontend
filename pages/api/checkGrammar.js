59dbaccad5d9da68bdca8baafc15a962dc32d056
philipp.herrchen <philipp.herrchen@gmail.com>
2020-10-15 00:14:51 +0200

    add docu

--- pages/api/checkGrammar.js
+++ pages/api/checkGrammar.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+//this represents the dummy api layer which could be used to send and manage
+// the requests to the backend(s) taking care about the audio processing
 export default (req, res) => {
   res.statusCode = 200;
   //fetch to node js backend
